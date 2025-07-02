4dba5c125d1e558ec997976f572dfeda0d226bd6
João Almeida <mr.johnplay0108@gmail.com>
2025-07-02 11:55:18 -0400

    resolvi o erro da sobreposicao dos card em relacao ao titulo na secaoContextualiacao e usei o js pra tirar n aparecer o html depois o css

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+// function aparecer(){
+//     const body = document.querySelector('body');
+//     body.style.display='block';
+// }
+
+// window.onload = function (){
+//     const body = document.querySelector('body');
+//     body.style.display = "block"
+// }
+
+window.addEventListener('load', () => {
+    document.querySelector('body').style.display = 'block'
+})
+
+// document.querySelector('body').addEventListener('load', () => {
+//     document.querySelector('body').style.display = 'block'
+// })
